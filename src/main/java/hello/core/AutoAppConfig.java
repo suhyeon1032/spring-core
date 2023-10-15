c146a88fb48c610634646ab885c5946019b67157
suhyeon1032 <daram_9@naver.com>
2023-10-15 22:48:09 +0900

    스프링 핵심 원리 - 컴포넌트 스캔 실습 내용

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // basePackages = "hello.core.member", // 탐색할 패키지의 시작 위치 지정
+        // basePackages를 지정하지 않으면 ComponentScan이 선언된 클래스(AutoAppConfig) "hello.core" package와 하위 package를 탐색한다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
